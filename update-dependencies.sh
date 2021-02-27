1c9f6defb4146a1ccd607138d7de609a2be61cfb
Oittaa <eero@oittaa.net>
2021-02-27 23:00:20 +0100

    Handle empty results

--- update-dependencies.sh
+++ update-dependencies.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 set -e
 latest_tag() {
-    printf "%s" "$(git ls-remote --tags --exit-code --refs "$1" | grep -oP '^[[:xdigit:]]+[[:space:]]+refs\/tags\/\Kv?[0-9\._-]*$' | sort -V | tail -n1)"
+    RESULT=$(git ls-remote --tags --exit-code --refs "$1" | grep -oP '^[[:xdigit:]]+[[:space:]]+refs\/tags\/\Kv?[0-9\._-]*$' | sort -V | tail -n1)
+    if [ ! "${RESULT}" ]
+    then
+        exit 1
+    fi
+    printf "%s" "${RESULT}"
 }
 TAG=$(latest_tag "https://github.com/ImageMagick/ImageMagick.git")
 sed -i "s/^ARG IM_VERSION=.*/ARG IM_VERSION=${TAG}/" Dockerfile
