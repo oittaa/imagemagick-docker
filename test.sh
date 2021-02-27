007b51d84c4597918d1320d96e126b8383c0e4c2
oittaa <oittaa@users.noreply.github.com>
2021-02-27 17:41:11 +0100

    Explicit platform (#2)
    * Explicit platform
    
    * Temp cache fix https://github.com/docker/build-push-action/issues/252

--- test.sh
+++ test.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 set -e
-docker run --rm -v $(pwd)/test:/imgs oittaa/imagemagick convert -resize 500 /imgs/test.jpg /imgs/test-resize.jpg
-docker run --rm -v $(pwd)/test:/imgs oittaa/imagemagick convert -resize 500 /imgs/test.jpg webp:/imgs/test-jpg.webp
-docker run --rm -v $(pwd)/test:/imgs oittaa/imagemagick convert -resize 500 /imgs/test.jpg avif:/imgs/test-jpg.avif
-docker run --rm -v $(pwd)/test:/imgs oittaa/imagemagick convert -resize 100 /imgs/test.png /imgs/test-resize.png
-docker run --rm -v $(pwd)/test:/imgs oittaa/imagemagick convert -resize 100 /imgs/test.png webp:/imgs/test-png.webp
-docker run --rm -v $(pwd)/test:/imgs oittaa/imagemagick convert -resize 100 /imgs/test.png avif:/imgs/test-png.avif
-docker run --rm -v $(pwd)/test:/imgs oittaa/imagemagick convert -resize 100 /imgs/test.heic /imgs/test-resize.heic
-docker run --rm -v $(pwd)/test:/imgs oittaa/imagemagick convert -resize 100 /imgs/test.heic webp:/imgs/test-heic.webp
-docker run --rm -v $(pwd)/test:/imgs oittaa/imagemagick convert -resize 100 /imgs/test.heic avif:/imgs/test-heic.avif
-docker run --rm -v $(pwd)/test:/imgs oittaa/imagemagick convert /imgs/test.pdf /imgs/test-pdf.png
-docker run --rm -v $(pwd)/test:/imgs oittaa/imagemagick convert /imgs/jpg-with-metadata.jpg /imgs/jpg-with-metadata.webp
+if [ ! "${PLATFORM}" ]
+then
+    PLATFORM="linux/amd64"
+fi
+docker run --rm --platform "${PLATFORM}" -v $(pwd)/test:/imgs oittaa/imagemagick convert -resize 500 /imgs/test.jpg /imgs/test-resize.jpg
+docker run --rm --platform "${PLATFORM}" -v $(pwd)/test:/imgs oittaa/imagemagick convert -resize 500 /imgs/test.jpg webp:/imgs/test-jpg.webp
+docker run --rm --platform "${PLATFORM}" -v $(pwd)/test:/imgs oittaa/imagemagick convert -resize 500 /imgs/test.jpg avif:/imgs/test-jpg.avif
+docker run --rm --platform "${PLATFORM}" -v $(pwd)/test:/imgs oittaa/imagemagick convert -resize 100 /imgs/test.png /imgs/test-resize.png
+docker run --rm --platform "${PLATFORM}" -v $(pwd)/test:/imgs oittaa/imagemagick convert -resize 100 /imgs/test.png webp:/imgs/test-png.webp
+docker run --rm --platform "${PLATFORM}" -v $(pwd)/test:/imgs oittaa/imagemagick convert -resize 100 /imgs/test.png avif:/imgs/test-png.avif
+docker run --rm --platform "${PLATFORM}" -v $(pwd)/test:/imgs oittaa/imagemagick convert -resize 100 /imgs/test.heic /imgs/test-resize.heic
+docker run --rm --platform "${PLATFORM}" -v $(pwd)/test:/imgs oittaa/imagemagick convert -resize 100 /imgs/test.heic webp:/imgs/test-heic.webp
+docker run --rm --platform "${PLATFORM}" -v $(pwd)/test:/imgs oittaa/imagemagick convert -resize 100 /imgs/test.heic avif:/imgs/test-heic.avif
+docker run --rm --platform "${PLATFORM}" -v $(pwd)/test:/imgs oittaa/imagemagick convert /imgs/test.pdf /imgs/test-pdf.png
+docker run --rm --platform "${PLATFORM}" -v $(pwd)/test:/imgs oittaa/imagemagick convert /imgs/jpg-with-metadata.jpg /imgs/jpg-with-metadata.webp
